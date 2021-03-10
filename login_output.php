abdbddc4f4c1ceb7f793ada95acd35ae490514d4
Ezura <ksw2070336@stu.o-hara.ac.jp>
2021-03-10 10:40:01 +0900

    コミット

--- login_output.php
+++ login_output.php
@@ -1,20 +1,50 @@
+<?php session_start(); ?>
+
+<?php
+    unset($_SESSION['customer']);
+
+    try {
+        $pdo = new PDO(
+            'mysql:dbname=testdb;host=localhost;charset=utf8mb4',
+            'root',
+            '');
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (Exception $e) {
+        echo '<span class="error">エラーがありました。</span><br>';
+        echo $e->getMessage();
+        exit();
+    }
+
+    $sql = "select * from customer where login = :login and password = :password";
+    $stm = $pdo->prepare($sql);
+    $stm->bindValue(':login', $_POST['login'],PDO::PARAM_STR);
+    $stm->bindValue(':password',$_POST['password'],PDO::PARAM_STR);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($result as $row) {
+        $_SESSION['customer'] = [
+            'id' => $row['id'], 'name' =>$row['name'],
+            'password' => $row['password']
+        ];
+    }
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
 </head>
 <body>
-    <!-- <?php
+    <?php
         if (isset($_SESSION['customer'])) {
             echo 'ログインしました。';
         } else {
             echo 'ログイン名またはパスワードが違います。';
         }
-    ?> -->
+    ?>
 
-    <p>ログインしました。</p>
 </body>
 </html>
