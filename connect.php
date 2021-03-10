0dd3ab682e20d29166f5e68b065f75e697381f1b
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-03-10 22:39:04 +0900

    修正

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $password = "1234";
 
 $opt = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMURATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
     PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
 ];
 $dbh = new PDO('mysql:host=localhost;dbname=sample_db', $user,$password,$opt);
--- list.php
+++ list.php
@@ -5,7 +5,7 @@ try {
     $password = "1234";
     $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EUMULATE_PREPARES => false,
+        PDO::ATTR_EMULATE_PREPARES => false,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
     ];
     $bdh = new PDO('mysql=localhost;dbname=sample_db',$user,$password,$opt);
