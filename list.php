c91e5052f15b8d53d455b4c8a4f0b1896b1292f5
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-03-10 23:31:08 +0900

    進捗

--- list.php
+++ list.php
@@ -7,14 +7,15 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+        // FETCH::FWTCH_ASSOC,
     ];
-    $bdh = new PDO('mysql=localhost;dbname=sample_db',$user,$password,$opt);
+    $dbh = new PDO('mysql:host=localhost;dbname=sample_db',$user,$password,$opt);
     $sql = 'SELECT title, author FROM books';
     $statement = $dbh->query($sql);
 
-    while($row = $statement->fetch()) {
-        echo "書籍名:" . str2html($row[0]) . "<br>";
-        echo "書籍名:" . str2html($row[0]) . "<br>";
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        echo "書籍名:" . str2html($row['title']) . "<br>";
+        // echo "書籍名:" . str2html($row['id']) . "<br><br>";
     }
 } catch (PDOException $e){
     echo "エラー!:" . str2html($e->getMessage()) . "<br>";
